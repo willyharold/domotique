3c7cce68fd287776ece2fa9bd07a8af13ec35520
harold <wtakoutsing@gmail.com>
2017-06-29 21:20:36 +0100

    ajout du bundle

---
+++ src/DomotiqueBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DomotiqueBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('DomotiqueBundle:Default:index.html.twig');
+    }
+}
---
+++ src/DomotiqueBundle/DomotiqueBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DomotiqueBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DomotiqueBundle extends Bundle
+{
+}
